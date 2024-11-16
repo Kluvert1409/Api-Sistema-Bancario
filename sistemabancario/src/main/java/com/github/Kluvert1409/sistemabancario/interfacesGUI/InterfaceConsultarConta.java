5f7d54d70d25496d4eb314d89010df6bee88f14f
Kluvert1409 <kluevertgabriel07@gmail.com>
2024-11-16 00:17:54 -0300

    commit

--- sistemabancario/src/main/java/com/github/Kluvert1409/sistemabancario/interfacesGUI/InterfaceConsultarConta.java
+++ sistemabancario/src/main/java/com/github/Kluvert1409/sistemabancario/interfacesGUI/InterfaceConsultarConta.java
@@ -1,15 +1,24 @@
 package com.github.Kluvert1409.sistemabancario.interfacesGUI;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class InterfaceConsultarConta extends JFrame {
 
     private JPanel panelBackGround;
+    private JButton botaoVoltar;
+    private ImageIcon imagemVoltar;
 
     public InterfaceConsultarConta() {
         configurarJanela();
+        adicionarBotoes();
         panelBackground();
+        adicionarAcoesBotoes();
     }
 
     private void configurarJanela() {
@@ -26,5 +35,45 @@ public class InterfaceConsultarConta extends JFrame {
         panelBackGround.setBackground(new Color(32, 32, 32));
         panelBackGround.setBounds(0, 0, 520, 600);
         add(panelBackGround);
+    }
+
+    private void adicionarBotoes() {
+        botaoVoltar = new JButton();
+        botaoVoltar.setBounds(50, 450, 80, 35);
+        botaoVoltar.setFont(new Font("Cascadian", Font.BOLD, 18));
+        botaoVoltar.setBackground(new Color(255, 102, 0));
+        botaoVoltar.setForeground(Color.white);
+        imagemVoltar = new ImageIcon(getClass().getResource("/imagens/back_48px.png"));
+        botaoVoltar.setIcon(imagemVoltar);
+        botaoVoltar.setBorder(new LineBorder(new Color(235, 94, 0), 1));
+        botaoVoltar.setFocusPainted(false);
+        add(botaoVoltar);
+    }
+
+    private void adicionarAcoesBotoes() {
+        botaoVoltar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                retornar();
+            }
+        });
+
+        botaoVoltar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                botaoVoltar.setBorder(new LineBorder(new Color(255, 229, 204), 2));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                botaoVoltar.setBorder(new LineBorder(new Color(235, 94, 0), 2));
+            }
+        });
+    }
+
+    private void retornar() {
+        InterfaceFuncionario novaJanela = new InterfaceFuncionario();
+        novaJanela.setVisible(true);
+        dispose();
     }
 }
